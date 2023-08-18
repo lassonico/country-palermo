9e0fb6c31388a007f6f5fea3d0ae7d8ae3118814
lassonico <sionvmx@gmail.com>
2023-08-18 10:38:09 -0500

    cambio de img

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,7 @@ export const metadata = {
   title: 'Country burger',
   description: 'Las mejores hamburguesas de Palermo',
   themeColor: '#141414'
+  // favicon: '/assets/logo/logo.svg'
 }
 
 // #EEB81D amarillo
@@ -16,6 +17,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+      <link
+        rel="icon"
+        href="/assets/logo/logo.svg"
+        type="image/svg"
+        sizes="<generated>"
+      />
+      </head>
       <body className={`${inter.className} backgroundpage font-body mx-auto md:hidden`}>
         <div className='w-[100%] h-[70vh] md:justify-center mb-10 py-3 pb-5 gap-1 z-10'>
           <div className='z-30 flex items-center justify-center mb-10 gap-2 fixed w-full mx-auto top-0 left-0 py-3 bg-[#141414]'>
